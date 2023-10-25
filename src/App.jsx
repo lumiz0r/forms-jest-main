125bc8ed0b85235c8a91c795770aa715d3309d40
Christian Cuesta <christianluminor@gmail.com>
2023-10-25 10:56:01 +0200

    Apollo Client base

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,27 @@ import React, { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import FormUserInformation from './components/FormUserInformation';
+import { gql, useQuery } from "@apollo/client";
+
+const HELLO_QUERY = gql`
+  query Query {
+    hello
+  }
+
+`;
 
 function App() {
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [userInfo, setUserInfo] = useState({});
+
+  const { loading, error, data } = useQuery(HELLO_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) {
+    console.log(error);
+    return <p>Error</p>;
+  }
 
   const handleSubmit = (userInformation) => {
     setUserInfo(userInformation);
@@ -15,6 +32,7 @@ function App() {
   return (
     <div>
       {isSubmitted ? <FormUserInformation {...userInfo} /> : <Form onSubmit={handleSubmit} />}
+      {data?.hello}
     </div>
   );
 }
